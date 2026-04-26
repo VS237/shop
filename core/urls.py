c1a3003cadd48ce9a472988bf90c5fa774a89bf7
VS237 <victorinsonhofo@gmail.com>
2026-04-26 19:07:50 +0200

    new

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import redirect
-from django.conf.urls.static import static
-from core import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -13,5 +11,3 @@ urlpatterns = [
     # Optional: Redirect root URL (/) to login automatically
     path('', lambda request: redirect('login')),
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
